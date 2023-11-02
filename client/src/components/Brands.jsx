020c153e3fe46f8b7559c2db2cfe2ad48fc402e1
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-02 10:51:14 +0200

    Create Brands component

---
+++ client/src/components/Brands.jsx
@@ -0,0 +1,44 @@
+export default function Brands() {
+    return (
+        <section id="brand" className="brand">
+            <div className="container">
+                <div className="brand-area">
+                    <div className="owl-carousel owl-theme brand-item">
+                        <div className="item">
+                            <a href="#">
+                                <img src="images/brand/br1.png" alt="brand-image" />
+                            </a>
+                        </div>
+                        <div className="item">
+                            <a href="#">
+                                <img src="images/brand/br2.png" alt="brand-image" />
+                            </a>
+                        </div>
+                        <div className="item">
+                            <a href="#">
+                                <img src="images/brand/br3.png" alt="brand-image" />
+                            </a>
+                        </div>
+                        <div className="item">
+                            <a href="#">
+                                <img src="images/brand/br4.png" alt="brand-image" />
+                            </a>
+                        </div>
+
+                        <div className="item">
+                            <a href="#">
+                                <img src="images/brand/br5.png" alt="brand-image" />
+                            </a>
+                        </div>
+
+                        <div className="item">
+                            <a href="#">
+                                <img src="images/brand/br6.png" alt="brand-image" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
