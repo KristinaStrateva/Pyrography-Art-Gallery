4303eb96cd2758ca6290443c987f8fc8ae7563e6
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-02 10:34:25 +0200

    Create HomeContent component

---
+++ client/src/components/HomeContent.jsx
@@ -0,0 +1,13 @@
+export default function HomeContent() {
+    return (
+        <div className="container">
+            <div className="welcome-hero-txt">
+                <h2>get your desired car in resonable price</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore   magna aliqua.
+                </p>
+                <button className="welcome-btn" onClick={window.location.href='#'}>contact us</button>
+            </div>
+        </div>
+    );
+}
