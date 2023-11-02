b9c139da8bf2d1b97c5b1f3e2b7ad7782b35b54a
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-02 10:43:58 +0200

    Create ClientsSay component

---
+++ client/src/components/ClientsSay.jsx
@@ -0,0 +1,75 @@
+export default function ClientsSay() {
+    return (
+        <section id="clients-say" className="clients-say">
+            <div className="container">
+                <div className="section-header">
+                    <h2>what our clients say</h2>
+                </div>
+                <div className="row">
+                    <div className="owl-carousel testimonial-carousel">
+                        <div className="col-sm-3 col-xs-12">
+                            <div className="single-testimonial-box">
+                                <div className="testimonial-description">
+                                    <div className="testimonial-info">
+                                        <div className="testimonial-img">
+                                            <img src="images/clients/c1.png" alt="image of clients person" />
+                                        </div>
+                                    </div>
+                                    <div className="testimonial-comment">
+                                        <p>
+                                            Sed ut pers unde omnis iste natus error sit voluptatem accusantium dolor laudan rem aperiam, eaque ipsa quae ab illo inventore verit.
+                                        </p>
+                                    </div>
+                                    <div className="testimonial-person">
+                                        <h2><a href="#">tomas lili</a></h2>
+                                        <h4>new york</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3 col-xs-12">
+                            <div className="single-testimonial-box">
+                                <div className="testimonial-description">
+                                    <div className="testimonial-info">
+                                        <div className="testimonial-img">
+                                            <img src="images/clients/c2.png" alt="image of clients person" />
+                                        </div>
+                                    </div>
+                                    <div className="testimonial-comment">
+                                        <p>
+                                            Sed ut pers unde omnis iste natus error sit voluptatem accusantium dolor laudan rem aperiam, eaque ipsa quae ab illo inventore verit.
+                                        </p>
+                                    </div>
+                                    <div className="testimonial-person">
+                                        <h2><a href="#">romi rain</a></h2>
+                                        <h4>london</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3 col-xs-12">
+                            <div className="single-testimonial-box">
+                                <div className="testimonial-description">
+                                    <div className="testimonial-info">
+                                        <div className="testimonial-img">
+                                            <img src="images/clients/c3.png" alt="image of clients person" />
+                                        </div>
+                                    </div>
+                                    <div className="testimonial-comment">
+                                        <p>
+                                            Sed ut pers unde omnis iste natus error sit voluptatem accusantium dolor laudan rem aperiam, eaque ipsa quae ab illo inventore verit.
+                                        </p>
+                                    </div>
+                                    <div className="testimonial-person">
+                                        <h2><a href="#">john doe</a></h2>
+                                        <h4>washington</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
