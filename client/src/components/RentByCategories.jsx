0df043c2b2a19accc8389668a95c7d2d0b747459
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-07 16:09:53 +0200

    Update RentByCategories.jsx

--- client/src/components/RentByCategories.jsx
+++ client/src/components/RentByCategories.jsx
@@ -3,34 +3,26 @@ export default function RentByCategories() {
         <div className="max-width-container">
             <div className="section-heading-section-heading">
                 <h1 className="section-heading-text Heading-2">
-                    <span>RENT YOUR CAR BY CATEGORIES</span>
+                    <span>BUY BY CATEGORIES</span>
                 </h1>
                 <span className="section-heading-text1">
                     <span>
-                        Rent car based on your preferences
+                        Make your purchase by your preferences
                     </span>
                 </span>
             </div>
             <div className="home-cards-container">
                 <div className="category-card-category-card">
-                    <img alt="image" src="images/pic2.jpg" className="category-card-image" />
-                    <span className="category-card-text"><span>Sedan</span></span>
+                    <img alt="image" src="images/categories/ideas_main_how-to-make-a-pyrography-home-sweet-home-sign.jpg" className="category-card-image" />
+                    <span className="category-card-text"><span>Home Decorations</span></span>
                 </div>
                 <div className="category-card-category-card">
-                    <img alt="image" src="images/hatchback.png" className="category-card-image" />
-                    <span className="category-card-text"><span>Hatchback</span></span>
+                    <img alt="image" src="images/categories/best-diy-wood-burning-ideas-designs-art-featured-homebnc-1024x536.jpg" className="category-card-image" />
+                    <span className="category-card-text"><span>Gift Sets</span></span>
                 </div>
                 <div className="category-card-category-card">
-                    <img alt="image" src="images/SW.png" className="category-card-image" />
-                    <span className="category-card-text"><span>SW</span></span>
-                </div>
-                <div className="category-card-category-card">
-                    <img alt="image" src="images/pic5.jpg" className="category-card-image" />
-                    <span className="category-card-text"><span>SUV/Mini SUV</span></span>
-                </div>
-                <div className="category-card-category-card">
-                    <img alt="image" src="images/pic6.jpg" className="category-card-image" />
-                    <span className="category-card-text"><span>Fast&Furiuos</span></span>
+                    <img alt="image" src="images/categories/Custom-Wood-Burning-Cutting-Board-madebybarb-4.jpg" className="category-card-image" />
+                    <span className="category-card-text"><span>Custom Text On Wood</span></span>
                 </div>
             </div>
         </div>
