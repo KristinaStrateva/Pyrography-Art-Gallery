1d8739da8bae098cc1133033d26e38dc81ffcde1
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-07 03:27:07 +0200

    Updates on main template

--- client/src/components/HomeMain.jsx
+++ client/src/components/HomeMain.jsx
@@ -63,10 +63,10 @@ export default function HomeMain() {
                 </div>
                 <div className="home-banner">
                     <div className="home-container05">
-                        <h3 className="home-text19 Heading-3">MOBILLIO</h3>
+                        <img src="images/1000_F_210640190_A393H4ZiIYDVR4iWZBwMvNdN8jxKKOoL.jpg" alt="logo" />
                         <span className="home-text20">
-                            <span></span>
-                            <span>furniture</span>
+                            {/* <span></span> */}
+                            {/* <span>Choose the best car for you</span> */}
                         </span>
                     </div>
                 </div>
