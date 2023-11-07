9047216e62b05063b97091a2c6ed1e922a0fbd41
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-07 02:20:53 +0200

    Extract Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+export default function Footer() {
+    return (
+        <div className="home-footer">
+            <div className="max-width-container">
+                <footer className="home-footer1">
+                    <div className="home-container09">
+                        <h3 className="home-text32 Heading-3">MOBILLIO</h3>
+                        <span className="home-text33">
+                            <span>4517 Washington Ave. Manchester, Kentucky 39495,</span>
+                            <br />
+                            <span>United States</span>
+                        </span>
+                        <span className="home-text36">(671) 555-0110</span>
+                        <span className="home-text37">contact@mobillio.com</span>
+                    </div>
+                    <div className="home-links-container">
+                        <div className="home-container10">
+                            <span className="home-text38">Categories</span>
+                            <a href="#" className="home-text39">Collections</a>
+                            <a href="#" className="home-text40">Desks</a>
+                            <a href="#" className="home-text41">Furniture</a>
+                            <a href="#" className="home-text42">Lamps</a>
+                            <a href="#" className="home-text43">Plants</a>
+                            <a href="#" className="home-text44">Decoration</a>
+                        </div>
+                        <div className="home-container11">
+                            <span className="home-text45">Company</span>
+                            <a href="#" className="home-text46">Shop</a>
+                            <a href="#" className="home-text47">Lookbook</a>
+                            <a href="#" className="home-text48">Specials</a>
+                            <a href="#" className="home-text49">About</a>
+                            <a href="#" className="home-text50">Blog</a>
+                        </div>
+                        <div className="home-container12">
+                            <span className="home-text51">Resources</span>
+                            <a href="#" className="home-text52">Contact us</a>
+                            <a href="#" className="home-text53">Order</a>
+                            <a href="#" className="home-text54">Track your order</a>
+                            <a href="#" className="home-text55">Shipping &amp; Delivery</a>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    );
+}
