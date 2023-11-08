6c8be28b5a05b08c9a4634398e99fd67bd441aa9
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-08 23:34:02 +0200

    Update Details.jsx

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -1,67 +1,69 @@
 export default function Details() {
     return (
-        <div className="section-container">
-            <div className="max-width-container">
-                <div className="section-heading-section-heading section-heading-root-className-name">
-                    <h1 className="section-heading-text Heading-2">
-                        <span>Our blog</span>
-                    </h1>
-                    <span className="section-heading-text1">
-                        <span>
-                            Read the latest news and furniture related articles
+        <div className="section-container column">
+            <div className="section-container">
+                <div className="max-width-container">
+                    <div className="section-heading-section-heading section-heading-root-className-name">
+                        <h1 className="section-heading-text Heading-2">
+                            <span>Our blog</span>
+                        </h1>
+                        <span className="section-heading-text1">
+                            <span>
+                                Read the latest news and furniture related articles
+                            </span>
                         </span>
-                    </span>
-                </div>
-                <div className="home-container08">
-                    <div className="blog-post-card-blog-post-card blog-post-card-root-className-name">
-                        <img alt="image" src="images/pic10.jpg" className="blog-post-card-image" />
-                        <div className="blog-post-card-container">
-                            <span className="blog-post-card-text">
-                                <span>Chic sofa designs for 2022</span>
-                            </span>
-                            <span className="blog-post-card-text1">
-                                <span>
-                                    Consectetur adipiscing elit duis tristique sollicitudin
-                                    nibh
-                                </span>
-                            </span>
-                            <a href="#" className="button">
-                                Read more
-                            </a>
-                        </div>
                     </div>
-                    <div className="blog-post-card-blog-post-card">
-                        <img alt="image" src="images/pic11.jpg" className="blog-post-card-image" />
-                        <div className="blog-post-card-container">
-                            <span className="blog-post-card-text">
-                                <span>Unique natural color combinations</span>
-                            </span>
-                            <span className="blog-post-card-text1">
-                                <span>
-                                    Consectetur adipiscing elit duis tristique sollicitudin
-                                    nibh
+                    <div className="home-container08">
+                        <div className="blog-post-card-blog-post-card blog-post-card-root-className-name">
+                            <img alt="image" src="images/pic10.jpg" className="blog-post-card-image" />
+                            <div className="blog-post-card-container">
+                                <span className="blog-post-card-text">
+                                    <span>Chic sofa designs for 2022</span>
                                 </span>
-                            </span>
-                            <a href="#" className="button">
-                                Read more
-                            </a>
+                                <span className="blog-post-card-text1">
+                                    <span>
+                                        Consectetur adipiscing elit duis tristique sollicitudin
+                                        nibh
+                                    </span>
+                                </span>
+                                <a href="#" className="button">
+                                    Read more
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                    <div className="blog-post-card-blog-post-card">
-                        <img alt="image" src="images/pic12.jpg" className="blog-post-card-image" />
-                        <div className="blog-post-card-container">
-                            <span className="blog-post-card-text">
-                                <span>Special combinations for nature lovers</span>
-                            </span>
-                            <span className="blog-post-card-text1">
-                                <span>
-                                    Consectetur adipiscing elit duis tristique sollicitudin
-                                    nibh
+                        <div className="blog-post-card-blog-post-card">
+                            <img alt="image" src="images/pic11.jpg" className="blog-post-card-image" />
+                            <div className="blog-post-card-container">
+                                <span className="blog-post-card-text">
+                                    <span>Unique natural color combinations</span>
                                 </span>
-                            </span>
-                            <a href="#" className="button">
-                                Read more
-                            </a>
+                                <span className="blog-post-card-text1">
+                                    <span>
+                                        Consectetur adipiscing elit duis tristique sollicitudin
+                                        nibh
+                                    </span>
+                                </span>
+                                <a href="#" className="button">
+                                    Read more
+                                </a>
+                            </div>
+                        </div>
+                        <div className="blog-post-card-blog-post-card">
+                            <img alt="image" src="images/pic12.jpg" className="blog-post-card-image" />
+                            <div className="blog-post-card-container">
+                                <span className="blog-post-card-text">
+                                    <span>Special combinations for nature lovers</span>
+                                </span>
+                                <span className="blog-post-card-text1">
+                                    <span>
+                                        Consectetur adipiscing elit duis tristique sollicitudin
+                                        nibh
+                                    </span>
+                                </span>
+                                <a href="#" className="button">
+                                    Read more
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
