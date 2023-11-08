5050398c691b5a204f8f551ad978b5dae348d3e9
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-08 14:22:32 +0200

    Extract About component form HomeMain component

--- client/src/components/HomeMain.jsx
+++ client/src/components/HomeMain.jsx
@@ -25,30 +25,6 @@ export default function HomeMain() {
                 <BuyByCategories />
 
                 <HomeBanner />
-                <div className="home-container06 max-width-container">
-                    <div className="home-container07">
-                        <span className="home-text23">
-                            <span>
-                                Mobilio Stores Inc. are unique reseller of modern furnitors,
-                                designer-made,
-                            </span>
-                            <br />
-                            <span>home-decoration items, since 1997.</span>
-                            <br />
-                            <span>
-                                Our legacy guarantees exceptional product quality, unique
-                                designs and special prices for all of our product line-up.
-                                Lorem ipsum, consectetur adipiscing elit duis tristique
-                                sollicitudin nibh sit amet commodo nulla facilisi nullam
-                                vehicula ipsum a arcu cursus vitae congue. Consectetur
-                                adipiscing elit duis tristique sollicitudin nibh sit amet
-                                commodo nulla facilisi nullam vehicula ipsum a arcu cursus
-                                vitae congue
-                            </span>
-                        </span>
-                        <button className="button">Read more</button>
-                    </div>
-                </div>
             </div>
             <div className="home-full-width-banner section-container">
                 <div className="home-left4">
