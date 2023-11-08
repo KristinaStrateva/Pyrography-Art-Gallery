42cc5df08cf68974616e6a3184fe577d0eb7eadc
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-08 23:32:05 +0200

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
     return (
         <div className="home-footer">
@@ -16,23 +18,23 @@ export default function Footer() {
                     <div className="home-links-container">
                         <div className="home-container10">
                             <span className="home-text38">Categories</span>
-                            <a href="#" className="home-text39">Home Decoration</a>
-                            <a href="#" className="home-text40">Gift Sets</a>
-                            <a href="#" className="home-text41">Custom text on wood</a>
+                            <Link to="/collection/homeDecorations" className="home-text39">Home Decoration</Link>
+                            <Link to="/collection/giftSets" className="home-text40">Gift Sets</Link>
+                            <Link to="/collection/customTextOnWood" className="home-text41">Custom text on wood</Link>
                         </div>
                         <div className="home-container11">
                             <span className="home-text45">Company</span>
-                            <a href="#" className="home-text46">Shop</a>
-                            <a href="#" className="home-text47">Lookbook</a>
-                            <a href="#" className="home-text48">Specials</a>
-                            <a href="#" className="home-text49">About</a>
+                            <Link to="#" className="home-text46">Shop</Link>
+                            <Link to="#" className="home-text47">Lookbook</Link>
+                            <Link to="#" className="home-text48">Specials</Link>
+                            <Link to="/about" className="home-text49">About</Link>
                         </div>
                         <div className="home-container12">
                             <span className="home-text51">Resources</span>
-                            <a href="#" className="home-text52">Contact us</a>
-                            <a href="#" className="home-text53">Order</a>
-                            <a href="#" className="home-text54">Track your order</a>
-                            <a href="#" className="home-text55">Shipping &amp; Delivery</a>
+                            <Link to="#" className="home-text52">Contact us</Link>
+                            <Link to="#" className="home-text53">Order</Link>
+                            <Link to="#" className="home-text54">Track your order</Link>
+                            <Link to="#" className="home-text55">Shipping &amp; Delivery</Link>
                         </div>
                     </div>
                 </footer>
