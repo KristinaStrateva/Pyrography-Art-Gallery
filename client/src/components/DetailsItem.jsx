3833220862d6d92c3fbfc10776f59c874384a30d
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-10 15:22:28 +0200

    Extract the DetailsItem css module

--- client/src/components/DetailsItem.jsx
+++ client/src/components/DetailsItem.jsx
@@ -1,21 +1,24 @@
+import styles from './DetailsItem.module.css';
+import mainStyle from '../App.module.css';
+
 export default function DetailsItem({
     name,
     imageUrl,
     description,
 }) {
     return (
-        <div className="blog-post-card-blog-post-card">
-            <img alt={name} src={imageUrl} className="blog-post-card-image" />
-            <div className="blog-post-card-container">
-                <span className="blog-post-card-text">
+        <div className={styles["blog-post-card-blog-post-card"]}>
+            <img alt={name} src={imageUrl} className={styles["blog-post-card-image"]} />
+            <div className={styles["blog-post-card-container"]}>
+                <span className={styles["blog-post-card-text"]}>
                     <span>{name}</span>
                 </span>
-                <span className="blog-post-card-text1">
+                <span className={styles["blog-post-card-text1"]}>
                     <span>
                         {description}
                     </span>
                 </span>
-                <a href="#" className="button">
+                <a href="#" className={mainStyle["button"]}>
                     More details
                 </a>
             </div>
