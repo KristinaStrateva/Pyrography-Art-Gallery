a087e14c31733900fb3cd76b8ed113ff49a1d54f
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-10 09:19:53 +0200

    Extract the Navigation css module

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,31 +1,32 @@
 import { Link } from 'react-router-dom';
+import styles from './Navigation.module.css';
 
 export default function Navigation({
     name
 }) {
     return (
-        <div className="home-navbar">
-            <header data-role="Header" className="home-header max-width-container">
-                <div className="home-navbar1">
-                    <div className="home-container01">
-                        <input type="text" placeholder="search" className="home-textinput input" />
+        <div className={styles["home-navbar"]}>
+            <header data-role="Header" className={styles["home-header", "max-width-container"]}>
+                <div className={styles["home-navbar1"]}>
+                    <div className={styles["home-container01"]}>
+                        <input type="text" placeholder="search" className={styles["home-textinput", "input"]} />
                     </div>
-                    <div className="home-middle">
-                        <ul className="home-left">
-                            <li className="navbar-link"><Link to="/collection/homeDecorations">HOME DECORATIONS</Link></li>
-                            
-                            <li className="navbar-link"><Link to="/collection/giftSets">GIFT SETS</Link></li>
-                            <li className="navbar-link"><Link to="/collection/customTextOnWood">CUSTOM TEXT ON WOOD</Link></li>
+                    <div className={styles["home-middle"]}>
+                        <ul className={styles["home-left"]}>
+                            <li className={styles["navbar-link"]}><Link to="/collection/homeDecorations">HOME DECORATIONS</Link></li>
+
+                            <li className={styles["navbar-link"]}><Link to="/collection/giftSets">GIFT SETS</Link></li>
+                            <li className={styles["navbar-link"]}><Link to="/collection/customTextOnWood">CUSTOM TEXT ON WOOD</Link></li>
                         </ul>
-                        <div className="navbar-logo-title navbar-link">
+                        <div className={styles["navbar-logo-title", "navbar-link"]}>
                             <Link to="/"><img src="images/2.png" alt="logo" /></Link>
                         </div>
-                        <ul className="home-right">
-                            <li className="navbar-link"><Link to="/users/login">LOGIN</Link></li>
-                            <li className="navbar-link"><Link to="/users/register">REGISTER</Link></li>
-                            {/* <li className="navbar-link"><a to="#"><img src="images/user-125-16.png" alt="user" /> ME</a></li> */}
-                            <li className="navbar-link"><Link to="#">Hello, {name}</Link></li>
-                            <li className="navbar-link"><Link to="/users/logout">LOGOUT</Link></li>
+                        <ul className={styles["home-right"]}>
+                            <li className={styles["navbar-link"]}><Link to="/users/login">LOGIN</Link></li>
+                            <li className={styles["navbar-link"]}><Link to="/users/register">REGISTER</Link></li>
+                            {/* <li className={styles["navbar-link"]}><a to="#"><img src="images/user-125-16.png" alt="user" /> ME</a></li> */}
+                            <li className={styles["navbar-link"]}><Link to="#">Hello, {name}</Link></li>
+                            <li className={styles["navbar-link"]}><Link to="/users/logout">LOGOUT</Link></li>
                         </ul>
                     </div>
                 </div>
