c47579493aab8b76279fc0b353bdf6d8b2382df2
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-10 10:37:41 +0200

    Extract the ShopByCategories css module

--- client/src/components/ShopByCategories.jsx
+++ client/src/components/ShopByCategories.jsx
@@ -1,29 +1,32 @@
+import styles from './ShopByCategories.module.css';
+import mainStyle from '../App.module.css';
+
 export default function ShopByCategories() {
     return (
-        <div className="section-container column">
-            <div className="max-width-container">
-                <div className="section-heading-section-heading">
-                    <h1 className="section-heading-text Heading-2">
+        <div className={`${mainStyle["section-container"]} ${mainStyle["column"]}`}>
+            <div className={mainStyle["max-width-container"]}>
+                <div className={styles["section-heading-section-heading"]}>
+                    <h1 className={`${styles["section-heading-text"]} ${mainStyle["Heading-2"]}`}>
                         <span>SHOP BY CATEGORIES</span>
                     </h1>
-                    <span className="section-heading-text1">
+                    <span className={styles["section-heading-text1"]}>
                         <span>
                             Make your purchase by your preferences
                         </span>
                     </span>
                 </div>
-                <div className="home-cards-container">
-                    <div className="category-card-category-card">
-                        <img alt="image" src="images/categories/homeDecoration/ideas_main_how-to-make-a-pyrography-home-sweet-home-sign.jpg" className="category-card-image" />
-                        <span className="category-card-text"><span>Home Decorations</span></span>
+                <div className={styles["home-cards-container"]}>
+                    <div className={styles["category-card-category-card"]}>
+                        <img alt="image" src="images/categories/homeDecoration/ideas_main_how-to-make-a-pyrography-home-sweet-home-sign.jpg" className={styles["category-card-image"]} />
+                        <span className={styles["category-card-text"]}><span>Home Decorations</span></span>
                     </div>
-                    <div className="category-card-category-card">
-                        <img alt="image" src="images/categories/giftSets/best-diy-wood-burning-ideas-designs-art-featured-homebnc-1024x536.jpg" className="category-card-image" />
-                        <span className="category-card-text"><span>Gift Sets</span></span>
+                    <div className={styles["category-card-category-card"]}>
+                        <img alt="image" src="images/categories/giftSets/best-diy-wood-burning-ideas-designs-art-featured-homebnc-1024x536.jpg" className={styles["category-card-image"]} />
+                        <span className={styles["category-card-text"]}><span>Gift Sets</span></span>
                     </div>
-                    <div className="category-card-category-card">
-                        <img alt="image" src="images/categories/customTextOnWood/Custom-Wood-Burning-Cutting-Board-madebybarb-4.jpg" className="category-card-image" />
-                        <span className="category-card-text"><span>Custom Text On Wood</span></span>
+                    <div className={styles["category-card-category-card"]}>
+                        <img alt="image" src="images/categories/customTextOnWood/Custom-Wood-Burning-Cutting-Board-madebybarb-4.jpg" className={styles["category-card-image"]} />
+                        <span className={styles["category-card-text"]}><span>Custom Text On Wood</span></span>
                     </div>
                 </div>
             </div>
