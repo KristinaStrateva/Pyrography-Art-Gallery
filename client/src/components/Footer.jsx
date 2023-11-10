c75b62ace9a4d48e6caa72c98b7cb562d96790af
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-10 10:52:49 +0200

    Extract the Footer css module

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,40 +1,42 @@
 import { Link } from 'react-router-dom';
+import styles from './Footer.module.css';
+import mainStyle from '../App.module.css';
 
 export default function Footer() {
     return (
-        <div className="home-footer">
-            <div className="max-width-container">
-                <footer className="home-footer1">
-                    <div className="home-container09">
-                        <h3 className="home-text32 Heading-3">VESSEY'S ART SHOP</h3>
-                        <span className="home-text33">
+        <div className={styles["home-footer"]}>
+            <div className={mainStyle["max-width-container"]}>
+                <footer className={styles["home-footer1"]}>
+                    <div className={styles["home-container09"]}>
+                        <h3 className={`${styles["home-text32"]} ${mainStyle["Heading-3"]}`}>VESSEY'S ART SHOP</h3>
+                        <span className={styles["home-text33"]}>
                             <span>25th September, Dobritch</span>
                             <br />
                             <span>Bulgaria</span>
                         </span>
-                        <span className="home-text36">(359) 8994 292 80</span>
-                        <span className="home-text37">veselina_hendry@gmail.com</span>
+                        <span className={styles["home-text36"]}>(359) 8994 292 80</span>
+                        <span className={styles["home-text37"]}>veselina_hendry@gmail.com</span>
                     </div>
-                    <div className="home-links-container">
-                        <div className="home-container10">
-                            <span className="home-text38">Categories</span>
-                            <Link to="/collection/homeDecorations" className="home-text39">Home Decoration</Link>
-                            <Link to="/collection/giftSets" className="home-text40">Gift Sets</Link>
-                            <Link to="/collection/customTextOnWood" className="home-text41">Custom text on wood</Link>
+                    <div className={styles["home-links-container"]}>
+                        <div className={styles["home-container10"]}>
+                            <span className={styles["home-text38"]}>Categories</span>
+                            <Link to="/collection/homeDecorations" className={styles["home-text39"]}>Home Decoration</Link>
+                            <Link to="/collection/giftSets" className={styles["home-text40"]}>Gift Sets</Link>
+                            <Link to="/collection/customTextOnWood" className={styles["home-text41"]}>Custom text on wood</Link>
                         </div>
-                        <div className="home-container11">
-                            <span className="home-text45">Company</span>
-                            <Link to="#" className="home-text46">Shop</Link>
-                            <Link to="#" className="home-text47">Lookbook</Link>
-                            <Link to="#" className="home-text48">Specials</Link>
-                            <Link to="/about" className="home-text49">About</Link>
+                        <div className={styles["home-container11"]}>
+                            <span className={styles["home-text45"]}>Company</span>
+                            <Link to="#" className={styles["home-text46"]}>Shop</Link>
+                            <Link to="#" className={styles["home-text47"]}>Lookbook</Link>
+                            <Link to="#" className={styles["home-text48"]}>Specials</Link>
+                            <Link to="/about" className={styles["home-text49"]}>About</Link>
                         </div>
-                        <div className="home-container12">
-                            <span className="home-text51">Resources</span>
-                            <Link to="#" className="home-text52">Contact us</Link>
-                            <Link to="#" className="home-text53">Order</Link>
-                            <Link to="#" className="home-text54">Track your order</Link>
-                            <Link to="#" className="home-text55">Shipping &amp; Delivery</Link>
+                        <div className={styles["home-container12"]}>
+                            <span className={styles["home-text51"]}>Resources</span>
+                            <Link to="#" className={styles["home-text52"]}>Contact us</Link>
+                            <Link to="#" className={styles["home-text53"]}>Order</Link>
+                            <Link to="#" className={styles["home-text54"]}>Track your order</Link>
+                            <Link to="#" className={styles["home-text55"]}>Shipping &amp; Delivery</Link>
                         </div>
                     </div>
                 </footer>
