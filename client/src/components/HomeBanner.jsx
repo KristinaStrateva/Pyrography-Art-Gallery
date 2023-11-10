6dd12915fdf0214aa4e5c12ce6ad20575c16573d
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-10 10:21:22 +0200

    Extract the HomeBanner css module

--- client/src/components/HomeBanner.jsx
+++ client/src/components/HomeBanner.jsx
@@ -1,12 +1,10 @@
+import styles from './HomeBanner.module.css';
+
 export default function HomeBanner() {
     return (
-        <div className="home-banner">
-            <div className="home-container05">
+        <div className={styles["home-banner"]}>
+            <div className={styles["home-container05"]}>
                 <img src="images/2.png" alt="logo" />
-                <span className="home-text20">
-                    {/* <span></span> */}
-                    {/* <span>Choose the best car for you</span> */}
-                </span>
             </div>
         </div>
     );
