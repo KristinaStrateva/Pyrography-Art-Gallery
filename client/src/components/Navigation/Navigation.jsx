f938f2c139ba5ee78b267f1bbd27c74372be248b
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-17 14:41:24 +0200

    Return accidently removed Navigation

---
+++ client/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import styles from './Navigation.module.css';
+import mainStyle from '../../App.module.css';
+
+export default function Navigation({
+    name
+}) {
+    return (
+        <div className={styles["home-navbar"]}>
+            <header data-role="Header" className={`${styles["home-header"]} ${mainStyle["max-width-container"]}`}>
+                <div className={styles["home-navbar1"]}>
+                    <div className={styles["home-container01"]}>
+                        <input type="text" placeholder="search" className={`${styles["home-textinput"]} ${mainStyle["input"]}`} />
+                    </div>
+                    <div className={styles["home-middle"]}>
+                        <ul className={styles["home-left"]}>
+                            <li className={styles["navbar-link"]}><Link to="/homeDecorations">HOME DECORATIONS</Link></li>
+
+                            <li className={styles["navbar-link"]}><Link to="/giftSets">GIFT SETS</Link></li>
+                            <li className={styles["navbar-link"]}><Link to="/customTextOnWood">CUSTOM TEXT ON WOOD</Link></li>
+                        </ul>
+                        <div className={`${styles["navbar-logo-title"]} ${styles["navbar-link"]}`}>
+                            <Link to="/"><img src="images/2.png" alt="logo" /></Link>
+                        </div>
+                        <ul className={styles["home-right"]}>
+                            <li className={styles["navbar-link"]}><Link to="/login">LOGIN</Link></li>
+                            <li className={styles["navbar-link"]}><Link to="/register">REGISTER</Link></li>
+                            <li className={styles["navbar-link"]}><Link to="#">Hello, {name}</Link></li>
+                            <li className={styles["navbar-link"]}><Link to="/logout">LOGOUT</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+}
