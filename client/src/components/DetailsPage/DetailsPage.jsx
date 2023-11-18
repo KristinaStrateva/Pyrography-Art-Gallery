e52ad3210b04d3f180700ce809050661209aa3e7
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-19 00:59:56 +0200

    Updates on details page content and css

--- client/src/components/DetailsPage/DetailsPage.jsx
+++ client/src/components/DetailsPage/DetailsPage.jsx
@@ -6,28 +6,24 @@ export default function DetailsPage() {
     return (
         <section className={styles.details}>
             <div className={styles["details-wrapper"]}>
-                <p className={styles["details-title"]}>Name: Mug pot</p>
                 <div className={styles["img-wrapper"]}>
                     <img src="" />
+                    <div>
+                        <p>0 Likes</p>
+                    </div>
                 </div>
                 <div className={styles["info-wrapper"]}>
-                    <table>
-                        <tr>
-                            <td>
-                                <p>
-                                    Price: <span className={styles["details-price"]}>€5</span>
-                                </p>
-                            </td>
-                        </tr>
-                    </table>
-                    <p>Description: <span className={styles["details-description"]}>Some description here...</span></p>
-                </div>
-
-                <div className="action-buttons">
-                    <Link to="/edit-item" className={styles["edit-btn"]}>Edit</Link>
-                    <Link to="/delete-item" className={styles["delete-btn"]}>Delete</Link>
-                    {/* Have to disable the Like button once it is clicked! */}
-                    <Link to="/like" className={styles["like-btn"]}>Like</Link>
+                    <div>
+                        <p className={styles["details-title"]}>Name: Mug pot</p>
+                        <p>Price: <span className={styles["details-price"]}>€5</span></p>
+                        <p>Description: <span className={styles["details-description"]}>Some description here...</span></p>
+                    </div>
+                    <div className={styles["action-buttons"]}>
+                        <Link to="/edit-item" className={styles["edit-btn"]}>Edit</Link>
+                        <Link to="/delete-item" className={styles["delete-btn"]}>Delete</Link>
+                        {/* Have to disable the Like button once it is clicked! */}
+                        <Link to="/like" className={styles["like-btn"]}>Like</Link>
+                    </div>
                 </div>
             </div>
         </section>
