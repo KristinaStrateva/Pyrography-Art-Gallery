716e407384dd09afc7f86f67e48550b7987a9e63
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-19 23:19:52 +0200

    Update itemsService.js with request from lib

--- client/src/services/itemsService.js
+++ client/src/services/itemsService.js
@@ -1,7 +1,9 @@
+import * as request from '../lib/request';
+
 const baseUrl = 'http://localhost:3030/jsonstore/items';
 
 export const getAllItems = async () => {
-    const response = await fetch(baseUrl);
+    const response = await request.get(baseUrl);
     const data = await response.json();
 
     const allItems = {...data.homeDecorations, ...data.giftSets, ...data.customTextOnWood};
@@ -10,33 +12,28 @@ export const getAllItems = async () => {
 };
 
 export const getAllFromCollection = async (collectionName) => {
-    const response = await fetch(`${baseUrl}/${collectionName}`);
+    const response = await request.get(`${baseUrl}/${collectionName}`);
     const data = await response.json();
 
     return data;
 };
 
 export const getItemById = async (collectionName, itemId) => {
-    const response = await fetch(`${baseUrl}/${collectionName}/${itemId}`);
+    const response = await request.get(`${baseUrl}/${collectionName}/${itemId}`);
     const data = await response.json();
 
     return data;
 };
 
 export const addItem = async (collectionName, itemData) => {
-    const body = {
+    const data = {
         ...itemData,
         price: Number(itemData.price),
         'purchasesAmount': 0,
         'likesAmount': 0
-    }
-    const response = await fetch(`${baseUrl}/${collectionName}`, {
-        method: 'POST',
-        header: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-    });
+    };
+    
+    const response = await request.post(`${baseUrl}/${collectionName}`, data);
 
     const result = await response.json();
 
