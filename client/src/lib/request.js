d40773047f3f54fcfc6498a6d007412ac139de02
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-19 23:18:32 +0200

    Create lib util for requests

---
+++ client/src/lib/request.js
@@ -0,0 +1,29 @@
+const buildOptions = (data) => {
+    const options = {};
+
+    if (data) {
+        options.header = {
+            'Content-Type': 'application/json'
+        };
+        options.body = JSON.stringify(data);
+    }
+
+    return options;
+};
+
+const request = async (method, url, data) => {
+    const response = await fetch(url, {
+        ...buildOptions(data),
+        method
+    });
+
+    const result = await response.json();
+
+    return result;
+};
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const del = request.bind(null, 'DELETE');
+export const patch = request.bind(null, 'PATCH');
