d64de3eb19996dff80cc5b9302257997875a29b6
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-22 01:32:06 +0200

    Conditionally show links from navbar

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,13 +1,17 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+import AuthContext from '../../contexts/authContext';
+
 import styles from './Navigation.module.css';
 import mainStyle from '../../App.module.css';
 
-export default function Navigation({
-    name
-}) {
+export default function Navigation() {
+    const { email, isAuthenticated } = useContext(AuthContext);
+
     return (
         <div className={styles["home-navbar"]}>
-            <header data-role="Header" className={`${styles["home-header"]} ${mainStyle["max-width-container"]}`}>
+            <header data-role="Header" className={`${styles["home-header"]} ${styles["max-width-container"]}`}>
                 <div className={styles["home-navbar1"]}>
                     <div className={styles["home-container01"]}>
                         <input type="text" placeholder="search" className={`${styles["home-textinput"]} ${mainStyle["input"]}`} />
@@ -22,13 +26,22 @@ export default function Navigation({
                         <div className={`${styles["navbar-logo-title"]} ${styles["navbar-link"]}`}>
                             <Link to="/"><img src="images/2.png" alt="logo" /></Link>
                         </div>
-                        <ul className={styles["home-right"]}>
-                            <li className={styles["navbar-link"]}><Link to="/login">LOGIN</Link></li>
-                            <li className={styles["navbar-link"]}><Link to="/register">REGISTER</Link></li>
-                            <li className={styles["navbar-link"]}><Link to="#">Hello, {name}</Link></li>
-                            <li className={styles["navbar-link"]}><Link to="/add-item">ADD ITEM</Link></li>
-                            <li className={styles["navbar-link"]}><Link to="/logout">LOGOUT</Link></li>
-                        </ul>
+
+                        {isAuthenticated && (
+                            <ul className={styles["home-right"]}>
+                                <li className={styles["navbar-link"]}><Link to="#">Hello, {email}</Link></li>
+                                <li className={styles["navbar-link"]}><Link to="/add-item">ADD ITEM</Link></li>
+                                <li className={styles["navbar-link"]}><Link to="/logout">LOGOUT</Link></li>
+                            </ul>
+                        )}
+
+                        {!isAuthenticated && (
+                            <ul className={styles["home-right"]}>
+                                <li className={styles["navbar-link"]}><Link to="/login">LOGIN</Link></li>
+                                <li className={styles["navbar-link"]}><Link to="/register">REGISTER</Link></li>
+                            </ul>
+                        )}
+
                     </div>
                 </div>
             </header>
