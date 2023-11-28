a527b6570a06fe97f1e4af7e2ccfbee31f912f95
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-29 00:16:26 +0200

    Correct edit form to controlled form

--- client/src/components/DetailsPage/EditPage/EditPage.jsx
+++ client/src/components/DetailsPage/EditPage/EditPage.jsx
@@ -4,7 +4,6 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 import * as itemsService from '../../../services/itemsService';
-import useForm from '../../../hooks/useForm';
 
 const EditFormKeys = {
     CollectionName: 'collectionName',
@@ -15,12 +14,16 @@ const EditFormKeys = {
 }
 
 export default function EditPage() {
-    const [item, setItem] = useState({});
-    const navigation = useNavigate();
-
+    const navigate = useNavigate();
     const { collectionName, itemId } = useParams();
-
     const [currentCollectionName, setCurrentCollectionName] = useState(collectionName);
+    const [item, setItem] = useState({
+        [EditFormKeys.CollectionName]: '',
+        [EditFormKeys.Name]: '',
+        [EditFormKeys.Price]: '',
+        [EditFormKeys.ImageUrl]: '',
+        [EditFormKeys.Description]: '',
+    });
 
     useEffect(() => {
         itemsService.getItemById(collectionName, itemId)
@@ -28,107 +31,37 @@ export default function EditPage() {
                 setItem(itemData);
 
                 setCurrentCollectionName(collectionName);
-
-                onChange({
-                    target: {
-                        name: EditFormKeys.CollectionName,
-                        value: collectionName,
-                    },
-                });
-                onChange({
-                    target: {
-                        name: EditFormKeys.Name,
-                        value: itemData.name,
-                    },
-                });
-                onChange({
-                    target: {
-                        name: EditFormKeys.Price,
-                        value: itemData.price,
-                    },
-                });
-                onChange({
-                    target: {
-                        name: EditFormKeys.ImageUrl,
-                        value: itemData.imageUrl,
-                    },
-                });
-                onChange({
-                    target: {
-                        name: EditFormKeys.Description,
-                        value: itemData.description,
-                    },
-                });
             })
             .catch(err => console.log(err));
     }, [collectionName, itemId]);
 
-    // useEffect(() => {
-    //     itemsService.getItemById(collectionName, itemId)
-    //         .then(setItem)
-    //         .catch(err => console.log(err));
-    // }, [itemId]);
+    const updateItemSubmitHandler = async (event) => {
+        event.preventDefault();
 
-    // useEffect(() => {
-    //     if (item) {
-    //       onChange({
-    //         target: {
-    //           name: EditFormKeys.CollectionName,
-    //           value: collectionName,
-    //         },
-    //       });
-    //       onChange({
-    //         target: {
-    //           name: EditFormKeys.Name,
-    //           value: item.name,
-    //         },
-    //       });
-    //       onChange({
-    //         target: {
-    //           name: EditFormKeys.Price,
-    //           value: item.price,
-    //         },
-    //       });
-    //       onChange({
-    //         target: {
-    //           name: EditFormKeys.ImageUrl,
-    //           value: item.imageUrl,
-    //         },
-    //       });
-    //       onChange({
-    //         target: {
-    //           name: EditFormKeys.Description,
-    //           value: item.description,
-    //         },
-    //       });
-    //     }
-    //   }, [item, collectionName]);
+        const values = Object.fromEntries(new FormData(event.currentTarget));
 
-    const updateItemSubmitHandler = async (values) => {
         try {
             await itemsService.updateItem(collectionName, itemId, values);
 
-            navigation(`/${collectionName}/${itemId}/details`);
+            navigate(`/${collectionName}/${itemId}/details`);
 
         } catch (error) {
             console.log(error);
-            // localStorage.removeItem('auth');
         }
     };
 
-    const { values, onChange, onSubmit } = useForm({
-        [EditFormKeys.CollectionName]: item.collectionName,
-        [EditFormKeys.Name]: item.name,
-        [EditFormKeys.Price]: item.price,
-        [EditFormKeys.ImageUrl]: item.imageUrl,
-        [EditFormKeys.Description]: item.description,
-    }, updateItemSubmitHandler);
+    const onChange = (event) => {
+        setItem(state => ({
+            ...state,
+            [event.target.name]: event.target.value,
+        }));
+    }
 
     return (
         <section className={styles.edit}>
             <div className={styles.form}>
                 <h2>Edit item</h2>
-                <form className={styles["edit-form"]} onSubmit={onSubmit}>
+                <form className={styles["edit-form"]} onSubmit={updateItemSubmitHandler}>
                     <select name="collectionName" value={currentCollectionName} onChange={onChange}>
                         <option value="homeDecorations">Home Decorations</option>
                         <option value="giftSets">Gift Sets</option>
@@ -138,31 +71,27 @@ export default function EditPage() {
                         type="text"
                         name="name"
                         placeholder="Name"
-                        // defaultValue={item.name}
-                        value={values[EditFormKeys.Name]}
+                        value={item[EditFormKeys.Name]}
                         onChange={onChange}
                     />
                     <input
                         type="number"
                         name="price"
                         placeholder="Price"
-                        // defaultValue={item.price}
-                        value={values[EditFormKeys.Price]}
+                        value={item[EditFormKeys.Price]}
                         onChange={onChange}
                     />
                     <input
                         type="text"
                         name="imageUrl"
                         placeholder="Image url starting with http:// or https://"
-                        // defaultValue={item.imageUrl}
-                        value={values[EditFormKeys.ImageUrl]}
+                        value={item[EditFormKeys.ImageUrl]}
                         onChange={onChange}
                     />
                     <textarea
                         name="description"
                         placeholder="Description..."
-                        // defaultValue={item.description}
-                        value={values[EditFormKeys.Description]}
+                        value={item[EditFormKeys.Description]}
                         onChange={onChange}
                     ></textarea>
 
