6c64dde146d43af9eaab20cc18275093ad4c8f56
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-12-01 01:15:14 +0200

    Add error boundary for white screen of death

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
+import ErrorBoundary from './components/ErrorBoundary';
 import { AuthProvider } from './contexts/authContext';
 import Path from './paths';
 
@@ -13,7 +14,6 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import Logout from './components/Logout/Logout';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import EditPage from './components/DetailsPage/EditPage/EditPage';
-import DeleteModal from './components/DetailsPage/DeleteModal/DeleteModal';
 import AddItemPage from './components/AddItemPage/AddItemPage';
 
 import styles from './App.module.css';
@@ -22,29 +22,31 @@ import AuthGuard from './components/Guards/AuthGuard';
 export default function App() {
 
     return (
-        <AuthProvider>
-            <div className={styles["home-container"]}>
-                <Navigation />
+        <ErrorBoundary>
+            <AuthProvider>
+                <div className={styles["home-container"]}>
+                    <Navigation />
 
-                <Routes>
-                    <Route path={Path.HomePage} element={<HomePage />} />
-                    <Route path={Path.LoginPage} element={<LoginPage />} />
-                    <Route path={Path.RegisterPage} element={<RegisterPage />} />
-                    <Route path={Path.AboutPage} element={<About />} />
-                    <Route path={Path.HomeDecorationsPage} element={<CollectionPage />} />
-                    <Route path={Path.GiftSetsPage} element={<CollectionPage />} />
-                    <Route path={Path.CustomTextOnWoodPage} element={<CollectionPage />} />
-                    <Route path={Path.DetailsPage} element={<DetailsPage />} />
+                    <Routes>
+                        <Route path={Path.HomePage} element={<HomePage />} />
+                        <Route path={Path.LoginPage} element={<LoginPage />} />
+                        <Route path={Path.RegisterPage} element={<RegisterPage />} />
+                        <Route path={Path.AboutPage} element={<About />} />
+                        <Route path={Path.HomeDecorationsPage} element={<CollectionPage />} />
+                        <Route path={Path.GiftSetsPage} element={<CollectionPage />} />
+                        <Route path={Path.CustomTextOnWoodPage} element={<CollectionPage />} />
+                        <Route path={Path.DetailsPage} element={<DetailsPage />} />
 
-                    <Route element={<AuthGuard />}>
-                        <Route path={Path.Logout} element={<Logout />} />
-                        <Route path={Path.EditPage} element={<EditPage />} />
-                        <Route path={Path.AddItemPage} element={<AddItemPage />} />
-                    </Route>
-                </Routes>
+                        <Route element={<AuthGuard />}>
+                            <Route path={Path.Logout} element={<Logout />} />
+                            <Route path={Path.EditPage} element={<EditPage />} />
+                            <Route path={Path.AddItemPage} element={<AddItemPage />} />
+                        </Route>
+                    </Routes>
 
-                <Footer />
-            </div>
-        </AuthProvider>
+                    <Footer />
+                </div>
+            </AuthProvider>
+        </ErrorBoundary>
     )
 };
---
+++ client/src/components/ErrorBoundary.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+export default class ErrorBoundary extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            hasError: false,
+        }
+    }
+
+    static getDerivedStateFromError(err) {
+        console.log(err);
+
+        return {
+            hasError: true,
+        }
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>404</h1>
+        }
+        
+        return this.props.children;
+    }
+}
