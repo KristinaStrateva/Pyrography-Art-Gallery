bb8b3ddecf7025416e021a8c2c98638bccbd5a1d
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-12-02 01:43:27 +0200

    Updating the about page

--- client/src/components/AboutPage/About.jsx
+++ client/src/components/AboutPage/About.jsx
@@ -1,5 +1,4 @@
 import styles from './About.module.css';
-// import mainStyle from '../App.module.css';
 
 export default function About() {
     return (
@@ -7,24 +6,43 @@ export default function About() {
             <div className={styles["home-container07"]}>
                 <span className={styles["home-text23"]}>
                     <span>
-                        Mobilio Stores Inc. are unique reseller of modern furnitors,
-                        designer-made,
+                        Hello, art enthusiasts! I'm Veselina Hendry, a passionate pyrography artist based in Dobrich, and I'm thrilled to welcome you to my online gallery. With three years of professional experience in the captivating world of pyrography, I've embarked on a journey to share my love for this unique art form with the world.
                     </span>
                     <br />
-                    <span>home-decoration items, since 1997.</span>
                     <br />
                     <span>
-                        Our legacy guarantees exceptional product quality, unique
-                        designs and special prices for all of our product line-up.
-                        Lorem ipsum, consectetur adipiscing elit duis tristique
-                        sollicitudin nibh sit amet commodo nulla facilisi nullam
-                        vehicula ipsum a arcu cursus vitae congue. Consectetur
-                        adipiscing elit duis tristique sollicitudin nibh sit amet
-                        commodo nulla facilisi nullam vehicula ipsum a arcu cursus
-                        vitae congue
+                        <b>About Me: </b>
+                        I discovered my love for pyrography three years ago, and ever since, I've been captivated by the dance of fire on wood. Each burn tells a story, and through my art, I aim to weave tales that resonate with the soul. Dobrich, with its rich cultural heritage, serves as my muse and a wellspring of inspiration for my creations.</span>
+                    <br />
+                    <br />
+                    <span>
+                        <b>The Art of Pyrography: </b>
+                        Pyrography, also known as wood burning, is an ancient art form that involves using a heated tool to create intricate designs on various wooden surfaces. What makes pyrography truly magical is the marriage of precision and creativity, as every stroke etches a moment in time onto the canvas of wood.
+                    </span>
+                    <br />
+                    <br />
+                    <span>
+                        <b>Your Art, Your Story: </b>
+                        Being a registered member unlocks the gateway to showcasing your artistic prowess. You can effortlessly add your articles, complete with brief descriptions, to three of our featured collections. Whether you're a seasoned pyrography artist or just starting your journey, this platform is a space for you to connect, inspire, and be inspired.
+                    </span>
+                    <br />
+                    <br />
+                    <span>
+                        <b>Join the Pyrography Community: </b>
+                        Beyond the strokes of my own creations, this gallery is a celebration of the diverse voices within the pyrography community. Join us in appreciating the beauty of burned lines, exploring the depths of creativity, and fostering connections that transcend geographical boundaries.
+                    </span>
+                    <br />
+                    <br />
+                    <span>
+                        <b>Get Started: </b>
+                        Ready to immerse yourself in the world of pyrography? Register today and become a part of a community that values artistic expression, craftsmanship, and the beauty of storytelling through wood burning. Let the journey begin!
+                    </span>
+                    <br />
+                    <br />
+                    <span>
+                        Thank you for being a part of PYROGRAPHY ART GALLERY. <b>Together, let's ignite the flames of creativity and make every burn count!</b>
                     </span>
                 </span>
-                {/* <button className={mainStyle["button"]}>Read more</button> */}
             </div>
         </div>
     );
