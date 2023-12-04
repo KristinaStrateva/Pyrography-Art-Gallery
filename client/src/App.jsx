0bfc5c960d78fff1a294a69939466463e4483489
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-12-04 11:11:32 +0200

    Updates on App styles and imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import ErrorBoundary from './components/ErrorBoundary';
 import { AuthProvider } from './contexts/authContext';
 import Path from './utils/paths';
 
-import Footer from './components/Footer/Footer';
+import AuthGuard from './components/Guards/AuthGuard';
 import HomePage from './components/HomePage/HomePage';
 import Navigation from './components/Navigation/Navigation';
-import CollectionPage from './components/CollectionPage/CollectionPage';
+import Footer from './components/Footer/Footer';
 import About from './components/AboutPage/About';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import Logout from './components/Logout/Logout';
+import MyItems from './components/MyItems/MyItems';
+import CollectionPage from './components/CollectionPage/CollectionPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import EditPage from './components/DetailsPage/EditPage/EditPage';
 import AddItemPage from './components/AddItemPage/AddItemPage';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 
 import styles from './App.module.css';
-import AuthGuard from './components/Guards/AuthGuard';
-import MyItems from './components/MyItems/MyItems';
-import { useEffect, useState } from 'react';
 
 export default function App() {
     const [error, setError] = useState(null);
@@ -60,17 +61,20 @@ export default function App() {
                                 <Route path={Path.AddItemPage} element={<AddItemPage />} />
                                 <Route path={Path.MyItems} element={<MyItems />} />
                             </Route>
+
+                            <Route path={Path.NotFound} element={<NotFoundPage />} />
                         </Routes>
 
                         <Footer />
                     </div>
                 </AuthProvider>
-            </ErrorBoundary>
+            </ErrorBoundary >
             {error && (
                 <div className={styles["error-container"]}>
                     <p>{error.message}</p>
                 </div>
-            )}
+            )
+            }
         </>
     )
 };
