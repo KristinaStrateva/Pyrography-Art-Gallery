7a9e1e9543b4f920228d77469d1d6f03c626d8e9
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-12-04 11:10:04 +0200

    Add ownerContext.jsx

---
+++ client/src/contexts/ownerContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState, useEffect, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+
+import * as itemsService from '../services/itemsService';
+import AuthContext from './authContext';
+import Path from '../utils/paths';
+
+const OwnerContext = createContext();
+
+export const OwnerGuardProvider = ({
+    children,
+    collectionName,
+    itemId
+}) => {
+    const [isOwner, setIsOwner] = useState(false);
+    const { userId } = useContext(AuthContext);
+
+    useEffect(() => {
+        if (userId) {
+            itemsService.getItemById(collectionName, itemId)
+                .then(result => setIsOwner(result._ownerId === userId))
+                .catch(err => {
+                    console.log(err)
+                    setIsOwner(false);
+                });
+        } else {
+            setIsOwner(false);
+        }
+    }, [userId, collectionName, itemId]);
+
+
+    console.log(isOwner)
+
+    if (!isOwner) {
+        return <Navigate to={Path.NotFound} />;
+    }
+
+    return (
+        <OwnerContext.Provider value={isOwner}>
+            {children}
+        </OwnerContext.Provider>
+    );
+};
+
+export default OwnerContext;
