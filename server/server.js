f06e22bae1ec300c96391249e82155f796a11d7d
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-12-09 02:54:59 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1321,8 +1321,8 @@
     var protectedData = {
         users: {
             "35c62d76-8152-4626-8712-eeb96381bea8": {
-                email: "veselina@abv.bg",
-                username: "Veselina",
+                email: "peter@abv.bg",
+                username: "Peter",
                 hashedPassword: "83313014ed3e2391aa1332615d2f053cf5c1bfe05ca1cbcb5582443822df6eb1"
             },
             "847ec027-f659-4086-8032-5173e2f9c93a": {
