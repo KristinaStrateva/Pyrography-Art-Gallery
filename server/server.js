8bcef0cc2b5779abcfb0e3b1f89c2c722e3f4ce1
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-01-25 12:46:51 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1442,7 +1442,7 @@
 
     const server = http__default['default'].createServer(requestHandler(plugins, services));
 
-    const port = 3030;
+    const port = process.env.PORT || 4000;
     server.listen(port);
     console.log(`Server started on port ${port}. You can make requests to http://localhost:${port}/`);
     console.log(`Admin panel located at http://localhost:${port}/admin`);
