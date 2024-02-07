991fcac7f2d18c43efd2cb30ff083dc7b3fbe4aa
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-07 23:55:37 +0200

    Add Mongoose and its configuration

--- mainServer/src/server.js
+++ mainServer/src/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const expressConfigurator = require('./config/expressConfigurator');
 const errorHandler = require('./middlewares/errorHandler');
+const dbConnect = require('./config/dbConfigurator');
 
 const PORT = process.env.PORT || 3500;
 
 const app = express();
 
 expressConfigurator(app);
+dbConnect(app);
 
 app.use(errorHandler);
 
