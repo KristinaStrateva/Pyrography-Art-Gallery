faa491d916aa0bf756befea8786aa04331d0c1bd
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-07 23:39:59 +0200

    Add errorHandler middleware

--- mainServer/src/server.js
+++ mainServer/src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const expressConfigurator = require('./config/expressConfigurator');
+const errorHandler = require('./middlewares/errorHandler');
 
 const PORT = process.env.PORT || 3500;
 
 const app = express();
 
 expressConfigurator(app);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
