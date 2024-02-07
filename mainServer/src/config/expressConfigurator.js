9fb0bf21b6abb0f64ecde0f7048593f1e62bd8e4
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-07 23:42:17 +0200

    Add CORS config

--- mainServer/src/config/expressConfigurator.js
+++ mainServer/src/config/expressConfigurator.js
@@ -1,14 +1,14 @@
 const express = require('express');
 // const path = require('path');
 const cookieParser = require('cookie-parser');
-// const cors = require('cors');
-// const corsOptions = require('./corsOptions');
+const cors = require('cors');
+const corsOptions = require('./corsOptions');
 const { logger } = require('../middlewares/logger');
 // const { setAuthentication } = require('../middlewares/authMiddleware');
 
 function expressConfigurator(app) {
     app.use(logger);
-    // app.use(cors(corsOptions));
+    app.use(cors(corsOptions));
     // app.use(express.static(path.resolve(__dirname, '../public')));
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
---
+++ mainServer/src/utils/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://www.pyrography-art-gallery.onrender.com',
+    'https://pyrography-art-gallery.onrender.com'
+];
+
+module.exports = allowedOrigins;
