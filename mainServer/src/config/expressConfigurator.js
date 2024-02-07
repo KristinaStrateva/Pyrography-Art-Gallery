7b77a0c7dd9dcc0ca07c4c75d269c410e58be81e
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-07 23:35:20 +0200

    Add Express config

---
+++ mainServer/src/config/expressConfigurator.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// const path = require('path');
+const cookieParser = require('cookie-parser');
+// const cors = require('cors');
+// const corsOptions = require('./corsOptions');
+// const { logger } = require('../middlewares/logger');
+// const { setAuthentication } = require('../middlewares/authMiddleware');
+
+function expressConfigurator(app) {
+    // app.use(logger);
+    // app.use(cors(corsOptions));
+    // app.use(express.static(path.resolve(__dirname, '../public')));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(cookieParser());
+    // app.use(setAuthentication);
+}
+
+module.exports = expressConfigurator;
--- mainServer/src/server.js
+++ mainServer/src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
+const expressConfigurator = require('./config/expressConfigurator');
+
 const PORT = process.env.PORT || 3500;
 
 const app = express();
 
+expressConfigurator(app);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
