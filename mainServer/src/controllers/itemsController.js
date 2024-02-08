c29308e40fd893149f30acb52b0f4d2c123d94fd
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-08 02:59:46 +0200

    Add itemsController.js

---
+++ mainServer/src/controllers/itemsController.js
@@ -0,0 +1,28 @@
+const asyncHandler = require('express-async-handler');
+const HomeDecorationsCollection = require('../models/HomeDecorationsCollection');
+const GiftSetsCollection = require('../models/GiftSetsCollection');
+const CustomItemsCollection = require('../models/CustomItemsCollection');
+
+// @desc Get last three added items
+// @route GET /
+// @access Private
+
+const getLastThreeItems = asyncHandler(async (req, res) => {
+    const homeDecorationsItems = await HomeDecorationsCollection.find().select('items').populate('item').lean();
+    const giftSetsItems = await GiftSetsCollection.find().select('items').populate('item').lean();
+    const customItemsItems = await CustomItemsCollection.find().select('items').populate('item').lean();
+
+    const allItems = [...homeDecorationsItems, ...giftSetsItems, ...customItemsItems];
+
+    if (!allItems) {
+        return res.status(400).json({message: 'No items found'});
+    }
+
+    const lastThreeItems = allItems.sort((a, b) => b.createdAt - a.createdAt).slice(0, 3);
+
+    res.json(lastThreeItems);
+});
+
+module.exports = {
+    getLastThreeItems,
+}
