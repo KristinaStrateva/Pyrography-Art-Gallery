5494b6f320702e2e72c7d880fda114b531e35e8c
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-08 03:03:50 +0200

    Update User model

---
+++ mainServer/src/middlewares/userValidator.js
@@ -0,0 +1,27 @@
+const registerValidation = [
+    body('username')
+        .notEmpty()
+        .withMessage('Username is required!')
+        .toLowerCase()
+        .trim()
+        .isLength({min: 8})
+        .withMessage('Username must be at least 8 characters!')
+        .isLength({max: 15})
+        .withMessage('Username must be less than 15 characters!'),
+    body('email')
+        .notEmpty()
+        .withMessage('Email is required!')
+        .toLowerCase()
+        .trim()
+        .isEmail()
+        .withMessage('Incorrect email address!'),
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required!')
+        .trim()
+        .isLength({ min: 8 })
+        .withMessage('Password must be at least 8 characters!')
+        .matches(/^(?=.*\d)(?=.*[A-Z]).+$/)
+        .withMessage('Password must contain at least 1 Capital letter and 1 digit!'),
+];
+    registerValidation,
--- mainServer/src/models/User.js
+++ mainServer/src/models/User.js
@@ -1,25 +1,26 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const Item = require('./Item');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: [true, 'Email is required!'],
-        match: [/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, 'Incorrect email address!'],
     },
     username: {
         type: String,
-        required: [true, 'Username is required!'],
-        minLength: [8, 'Username must be at least 8 characters!'],
-        maxLength: [15, 'Username must be less than 15 characters!'],
         unique: true,
     },    
     password: {
         type: String,
-        required: [true, 'Password is required!'],
-        minLength: [8, 'Password must be at least 4 characters!'],
-        maxLength: [25, 'Password must be less than 25 characters!'],
-    }
+    },
+    items: [
+        {
+            item: {
+                type: mongoose.Types.ObjectId,
+                ref: Item,
+            }
+        }
+    ]
 });
 
 userSchema.virtual('rePassword')
