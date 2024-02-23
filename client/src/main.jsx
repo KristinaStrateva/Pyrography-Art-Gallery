5249885e776e708488e6af93309dc8a0536df45d
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-24 00:20:47 +0200

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Router, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <Router>
+        <Routes>
             <Route path='/*' element={<App />} />
-        </Router>
+        </Routes>
     </BrowserRouter>
 )
