9fe600fde44083ce0107db9f640401fd7914ce2e
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-24 00:23:02 +0200

    Enable logger in expressConfigurator.js

--- server/src/config/expressConfigurator.js
+++ server/src/config/expressConfigurator.js
@@ -1,23 +1,19 @@
 require('dotenv').config();
 
 const express = require('express');
-// const path = require('path');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./corsOptions');
-// const { logger } = require('../middlewares/logger');
-// const { setAuthentication } = require('../middlewares/authMiddleware');
+const { logger } = require('../middlewares/logger');
 
 function expressConfigurator(app) {
     console.log(`Node.js execution mode: ${process.env.NODE_ENV}`);
 
-    // app.use(logger);
+    app.use(logger);
     app.use(cors(corsOptions));
-    // app.use(express.static(path.resolve(__dirname, '../public')));
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
     app.use(cookieParser());
-    // app.use(setAuthentication);
 }
 
 module.exports = expressConfigurator;
--- server/src/middlewares/logger.js
+++ server/src/middlewares/logger.js
@@ -19,7 +19,7 @@ const logEvents = async (message, logFileName) => {
 }
 
 const logger = (req, res, next) => {
-    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, `reqLog.log`);
+    // logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, `reqLog.log`);
 
     console.log(`${req.method} ${req.path}`);
 
