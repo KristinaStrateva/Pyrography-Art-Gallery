602f531b5f0dc42cfbf9f37bb05cecb0aa704e7e
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-28 16:55:21 +0200

    Update OwnerGuard component

--- client/src/components/Guards/AuthGuard.jsx
+++ client/src/components/Guards/AuthGuard.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Navigate, Outlet, useNavigate, useParams } from "react-router-dom";
+import { Navigate, Outlet, useParams } from "react-router-dom";
 
 import AuthContext from "../../contexts/authContext";
 import * as itemsService from "../../services/itemsService";
@@ -9,6 +9,7 @@ export const AuthGuard = () => {
     const { isAuthenticated } = useContext(AuthContext);
 
     if (!isAuthenticated) {
+        console.log('some')
         return <Navigate to={Path.LoginPage} />
     }
 
@@ -18,15 +19,28 @@ export const AuthGuard = () => {
 export const OwnerGuard = () => {
     const { userId } = useContext(AuthContext);
     const { collectionName, itemId } = useParams();
-    const navigate = useNavigate();
+    const [hasPermission, setHasPermission] = useState(true);
 
-    itemsService.getItemById(collectionName, itemId)
-            .then(item => {
+    console.log(userId);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const item = await itemsService.getItemById(collectionName, itemId);
+
                 if (userId !== item.owner) {
-                    return navigate(Path.NotFound);
+                    setHasPermission(false);
                 }
-            })
-            .catch(err => { throw err });
 
-    return <Outlet />
+            } catch (error) {
+                setHasPermission(false);
+
+                throw err;
+            }
+        };
+
+        fetchData();
+    }, [collectionName, itemId, userId]);
+
+    return hasPermission ? <Outlet /> : <Navigate to={Path.NotFound} />;
 }
