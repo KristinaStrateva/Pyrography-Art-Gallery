d97f785db74b9fac38facf06a8b222ea72bd879b
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-03-30 14:43:10 +0200

    Update Spinner html and css

--- client/src/components/Spinner/Spinner.jsx
+++ client/src/components/Spinner/Spinner.jsx
@@ -4,8 +4,10 @@ import mainStyle from '../../App.module.css';
 export default function Spinner() {
     return (
         <div className={`${mainStyle["section-container"]} ${mainStyle["column"]}`}>
-            <div className={mainStyle["max-width-container"]}>
-                <div className={styles.loader}></div>
+            <div className={mainStyle["section-container"]}>
+                <div className={mainStyle["max-width-container"]}>
+                    <div className={styles.loader}></div>
+                </div>
             </div>
         </div >
     )
