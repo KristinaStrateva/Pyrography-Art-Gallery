9792493594f3da586ab459efcbbcfc5a6dca79be
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-03-30 14:47:46 +0200

    Add spinner to MyItems page

--- client/src/components/MyItems/MyItems.jsx
+++ client/src/components/MyItems/MyItems.jsx
@@ -7,43 +7,58 @@ import transformCollectionName from "../../utils/transformCollectionName";
 
 import styles from './MyItems.module.css';
 import mainStyle from '../../App.module.css';
+import Spinner from "../Spinner/Spinner";
 
 export default function MyItems() {
+    const [isLoading, setIsLoading] = useState(true);
     const [myItems, setMyItems] = useState([]);
 
     const { accessToken } = useContext(AuthContext);
 
     useEffect(() => {
         itemsService.getMyItems(accessToken)
-            .then(result => setMyItems(result))
-            .catch(err => {throw err});
+            .then(result => {
+                setIsLoading(false);
+                setMyItems(result);
+            })
+            .catch(err => {
+                setIsLoading(false);
+                throw err;
+            });
     }, []);
 
     return (
-        <div className={styles.wrapper}>
+        <>
+            {isLoading && <Spinner />}
 
-            {myItems.length > 0 && (
-                <>
-                    <h1 className={`${mainStyle["section-heading-text"]} ${mainStyle["Heading-2"]}`}>
-                        <span>MY ITEMS</span>
-                    </h1>
-                    <div className={styles["main-section"]}>
-                        {myItems.map(item => {
-                            const collectionName = transformCollectionName(item.fromCollection.name);
+            {!isLoading && (
+                <div className={styles.wrapper}>
 
-                            return (
-                                <div key={item._id} className={styles["category-card-category-card"]}>
-                                    <img alt="image" src={item.imageUrl} className={styles["category-card-image"]} />
-                                    <h5>{item.name}</h5>
-                                    <Link to={`/${collectionName}/${item._id}/details`} className={mainStyle.button}>More details</Link>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </>
+                    {myItems.length > 0 && (
+                        <>
+                            <h1 className={`${mainStyle["section-heading-text"]} ${mainStyle["Heading-2"]}`}>
+                                <span>MY ITEMS</span>
+                            </h1>
+                            <div className={styles["main-section"]}>
+                                {myItems.map(item => {
+                                    const collectionName = transformCollectionName(item.fromCollection.name);
+
+                                    return (
+                                        <div key={item._id} className={styles["category-card-category-card"]}>
+                                            <img alt="image" src={item.imageUrl} className={styles["category-card-image"]} />
+                                            <h5>{item.name}</h5>
+                                            <Link to={`/${collectionName}/${item._id}/details`} className={mainStyle.button}>More details</Link>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </>
+                    )}
+
+                    {!myItems.length && <p>You don't have any items added yet!</p>}
+                    
+                </div>
             )}
-
-            {!myItems.length && <p>You don't have any items added yet!</p>}
-        </div>
+        </>
     );
 }
